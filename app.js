6a3708228aedc12d2a2bb43022c0a455e8c764c0
Akarsh <Akarsh924sri@mgail.com>
2024-11-23 21:18:23 +0530

    Retrieving Data From API

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 const port = 3000; 
-const API_URL = 'https://api.tvmaze.com/search/shows';
+const API_URL = 'https://api.tvmaze.com/search/shows?q='
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,9 +13,10 @@ app.get('/', (req, res) => {
     res.render('home.ejs');
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const movie = req.body.movie;
-    res.render('home.ejs');
+    const result = await axios.get(API_URL + movie);
+    res.render('home.ejs', {data : result.data}, console.log(result.data));   
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
